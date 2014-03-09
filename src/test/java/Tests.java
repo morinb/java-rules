d07daa43a76cf52ec8a33853353b5b5091bbf66c
Baptiste MORIN <morin.baptiste@gmail.com>
2014-03-09 19:23:57 +0100

    update

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -34,86 +34,86 @@ import static org.junit.Assert.assertNotNull;
 @ContextConfiguration(locations = {"/spring-context-for-tests.xml"})
 public class Tests {
 
-   Logger LOG = Logger.getLogger(Tests.class);
+    Logger LOG = Logger.getLogger(Tests.class);
 
-   @Autowired
-   private Engine engine;
+    @Autowired
+    private Engine engine;
 
-   @Autowired
-   private ApplicationContext context;
+    @Autowired
+    private ApplicationContext context;
 
-   @Test
-   public void firstTest() {
-      assertEquals("2.0 = 2.0", 2.0, 2.0, 0.001);
-   }
-
-
-   @Test
-   public void testKeyPair() {
-      KeyPair<String, String> key1 = new KeyPairImpl<String, String>("first", "second");
-      KeyPair<String, String> key2 = new KeyPairImpl<String, String>("first", "second");
-
-      assertEquals(key1, key2);
-
-      assertEquals("first", key1.getFirst());
-      assertEquals("second", key1.getSecond());
-
-   }
-
-   @Test
-   public void testRulesLoading() throws IllegalAccessException, IOException, InstantiationException, URISyntaxException {
-
-      URL url = getClass().getResource("rules"); // Search in resources/rules directory
-
-      File directory = new File(url.toURI());
-
-      RuleLoader grl = new GroovyRulesLoader(new File[]{directory});
-
-      Collection<Rule> rules = grl.load();
-
-      assertNotNull("The loaded rules collection is null.", rules);
-      assertNotNull("Directory should not be empty", directory.listFiles());
-      assertEquals("There is not as much Rules as script in the directory", directory.listFiles().length, rules.size());
-
-   }
-
-   @Test
-   public void testSpringIoC() {
-      assertNotNull("engine should not be null.", engine);
-      assertNotNull("context should not be null", context);
-   }
-
-   @Test
-   public void testEngine() {
-
-      try {
-         Entry sampleEntry = (Entry) context.getBean("test-Entry");
-         Collection<Rule> sampleRules =  ((RuleLoader) context.getBean("test-RuleLoader")).load();
-
-         Map<KeyPair<Entry,Rule>,Result> results = engine.process(Lists.newArrayList(sampleEntry), sampleRules);
+    @Test
+    public void firstTest() {
+        assertEquals("2.0 = 2.0", 2.0, 2.0, 0.001);
+    }
 
 
-         assertNotNull(results);
-         Rule rule = sampleRules.iterator().next();
+    @Test
+    public void testKeyPair() {
+        KeyPair<String, String> key1 = new KeyPairImpl<String, String>("first", "second");
+        KeyPair<String, String> key2 = new KeyPairImpl<String, String>("first", "second");
 
-         KeyPair<Entry, Rule> kp = new KeyPairImpl<Entry, Rule>(sampleEntry, rule);
+        assertEquals(key1, key2);
 
-         Result result = results.get(kp);
-         assertNotNull(result);
+        assertEquals("first", key1.getFirst());
+        assertEquals("second", key1.getSecond());
 
-         assertEquals("SimpleRule severity should have been 0", 0,  result.getStatus().getSeverity());
+    }
 
-         ResultFormatter resultFormatter = (ResultFormatter) context.getBean("test-ResultFormatter");
+    @Test
+    public void testRulesLoading() throws IllegalAccessException, IOException, InstantiationException, URISyntaxException {
 
-         LOG.info(resultFormatter.format(result));
+        URL url = getClass().getResource("rules"); // Search in resources/rules directory
 
-      } catch (IOException e) {
-         e.printStackTrace();
-      } catch (IllegalAccessException e) {
-         e.printStackTrace();
-      } catch (InstantiationException e) {
-         e.printStackTrace();
-      }
+        File directory = new File(url.toURI());
 
-   }
+        RuleLoader grl = new GroovyRulesLoader(new File[]{directory});
+
+        Collection<Rule> rules = grl.load();
+
+        assertNotNull("The loaded rules collection is null.", rules);
+        assertNotNull("Directory should not be empty", directory.listFiles());
+        assertEquals("There is not as much Rules as script in the directory", directory.listFiles().length, rules.size());
+
+    }
+
+    @Test
+    public void testSpringIoC() {
+        assertNotNull("engine should not be null.", engine);
+        assertNotNull("context should not be null", context);
+    }
+
+    @Test
+    public void testEngine() {
+
+        try {
+            Entry sampleEntry = (Entry) context.getBean("test-Entry");
+            Collection<Rule> sampleRules = ((RuleLoader) context.getBean("test-RuleLoader")).load();
+
+            Map<KeyPair<Entry, Rule>, Result> results = engine.process(Lists.newArrayList(sampleEntry), sampleRules);
+
+
+            assertNotNull(results);
+            Rule rule = sampleRules.iterator().next();
+
+            KeyPair<Entry, Rule> kp = new KeyPairImpl<Entry, Rule>(sampleEntry, rule);
+
+            Result result = results.get(kp);
+            assertNotNull(result);
+
+            assertEquals("SimpleRule severity should have been 0", 0, result.getStatus().getSeverity());
+
+            ResultFormatter resultFormatter = (ResultFormatter) context.getBean("test-ResultFormatter");
+
+            LOG.info(resultFormatter.format(result));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
